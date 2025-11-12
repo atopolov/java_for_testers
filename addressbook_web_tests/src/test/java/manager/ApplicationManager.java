4d32ff1c71f2661071569f6e9a9110bd2a9fe4e2
Andrei Topolov <atopolov@gmail.com>
2025-11-12 10:05:41 +0300

    Выполнено задание №8
    1. Добавлена возможность запуска тестов в разных браузерах (Firefox по умолчанию и Chrome).
    2. Также значения URL и разрешения экрана при запуске браузера вынесены в глобальные переменные
    3. Для запуска тестов в Chrome были добавлены ожидания, так как браузер не успевал найти элемент name="group_name"

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -4,19 +4,34 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.time.Duration;
+
 public class ApplicationManager {
+
+    public static final String URL = "http://localhost/addressbook/";
+    public static final Dimension DIMENSION = new Dimension(1650, 818);
+
     protected WebDriver driver;
     private LoginHelper session;
     private GroupHelper groups;
 
-    public void init() {
+    public void init(String browser) {
         if (driver == null) {
-            driver = new FirefoxDriver();
+            if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Введено неподдерживаемое значение браузера: %s", browser));
+            }
             Runtime.getRuntime().addShutdownHook(new Thread(() -> driver.quit()));
-            driver.get("http://localhost/addressbook/");
-            driver.manage().window().setSize(new Dimension(1650, 818));
+
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+            driver.manage().window().setSize(DIMENSION);
+            driver.get(URL);
             session().login("admin", "secret");
         }
     }
--- addressbook_web_tests/src/test/java/manager/HelperBase.java
+++ addressbook_web_tests/src/test/java/manager/HelperBase.java
@@ -1,6 +1,11 @@
 package manager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HelperBase {
     protected final ApplicationManager manager;
@@ -10,10 +15,14 @@ public class HelperBase {
     }
 
     protected void click(By locator) {
+        WebDriverWait wait = new WebDriverWait(manager.driver, Duration.ofSeconds(5));
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
         manager.driver.findElement(locator).click();
     }
 
     protected void type(By locator, String text) {
+        WebDriverWait wait = new WebDriverWait(manager.driver, Duration.ofSeconds(5));
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
         click(locator);
         manager.driver.findElement(locator).clear();
         manager.driver.findElement(locator).sendKeys(text);
