263537e115b10ea43bdc03519e80a51ac3f32318
Andrei Topolov <atopolov@gmail.com>
2025-12-15 09:13:35 +0300

    Выполнено задание №17
    Подготовлен новый проект для тестирования MantisBT

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -0,0 +1,62 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+import java.util.Properties;
+
+public class ApplicationManager {
+
+    private static final Dimension WINDOW_SIZE = new Dimension(1650, 818);
+    private static final int IMPLICIT_WAIT_SECONDS = 5;
+
+    private WebDriver driver;
+    private String browser;
+    private Properties properties;
+
+    public void init(String browser, Properties properties) {
+        this.properties = properties;
+        this.browser = browser;
+    }
+
+    public WebDriver driver() {
+        if (driver == null) {
+            driver = createDriver(browser);
+            registerShutdownHook();
+            configureDriver();
+            openHomePage();
+        }
+        return driver;
+    }
+
+    public void openHomePage() {
+        driver.get(properties.getProperty("web.baseUrl"));
+    }
+
+    private WebDriver createDriver(String browser) {
+        return switch (browser.toLowerCase()) {
+            case "firefox" -> new FirefoxDriver();
+            case "chrome" -> new ChromeDriver();
+            default -> throw new IllegalArgumentException(
+                    String.format("Unsupported browser: %s", browser)
+            );
+        };
+    }
+
+    private void configureDriver() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT_SECONDS));
+        driver.manage().window().setSize(WINDOW_SIZE);
+    }
+
+    private void registerShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                if (driver != null) driver.quit();
+            } catch (Exception ignored) {
+            }
+        }));
+    }
+}
